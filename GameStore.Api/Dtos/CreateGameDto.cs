645eed4702af7de53f187614ecc734d7bbe594cd
AbhishekSinghDev <abhishek.code.tech@gmail.com>
2026-09-12 11:47:32 +0530

    refactor: enhance data validation in CreateGameDto and UpdateGameDto

--- GameStore.Api/Dtos/CreateGameDto.cs
+++ GameStore.Api/Dtos/CreateGameDto.cs
@@ -1,8 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace GameStore.Api.Dtos;
 
 public record class CreateGameDto(
-    string Name,
-    string Genre,
-    decimal Price,
+    [Required][StringLength(40)] string Name,
+    [Required][StringLength(20)] string Genre,
+    [Required][Range(0, 1000)] decimal Price,
     DateOnly ReleaseDate
 );
